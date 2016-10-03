c222885b4b2a21456493ff1e8d574077cdedddd7
ic070_23 <ic070_23@aguacate.ece.uprm.edu>
2016-10-03 14:46:23 -0400

    First commit?

---
+++ src/positionListLLDirect/PositionListElementsBackwardIterator.java
@@ -0,0 +1,54 @@
+package positionListLLDirect;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import positionInterfaces.Position;
+import positionInterfaces.PositionList;
+
+public class PositionListElementsBackwardIterator<T> implements Iterator<T>{
+
+	protected Position<T> current;
+	protected PositionList<T> theList;
+	
+	public PositionListElementsBackwardIterator() {
+		//for inheritance purposes
+	}
+	public PositionListElementsBackwardIterator(PositionList<T> list) {
+		// TODO Auto-generated constructor stub
+		theList = list;
+		if(theList.isEmpty())
+			current = null;
+		else
+			current = theList.last();
+		
+		
+	}
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		System.out.println("jajaja");
+		System.out.println("hasNext       of Parent");
+		return current!=null;
+		
+	}
+
+	@Override
+	public T next() throws NoSuchElementException {
+		System.out.println("next of parent");
+		// TODO Auto-generated method stub
+		if(!hasNext()) throw new NoSuchElementException();
+		Position<T> ptr = current;
+		try{	
+		
+			current = theList.prev(current);
+			
+		}catch(Exception e){
+			
+			current = null;
+		}
+		
+		return ptr.element();
+	}
+
+}
