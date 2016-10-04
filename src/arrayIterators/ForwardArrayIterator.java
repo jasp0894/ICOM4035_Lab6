3203bca3043d41b70ce88050810b3abf51179a7c
J.A. Sanchez Perez <J.A. Sanchez Perez@JASP>
2016-10-03 21:47:27 -0400

    re-doing excercise 1

--- src/arrayIterators/ForwardArrayIterator.java
+++ src/arrayIterators/ForwardArrayIterator.java
@@ -7,23 +7,25 @@ public class ForwardArrayIterator<E> implements Iterator<E> {
 
 	private E[] arr;    // the array to iterate over
 	// other internal fields...
+	private int current;
 	
 	public ForwardArrayIterator(E[] arr) { 
 		this.arr = arr; 
+		current =0;
 		// initialize other internal fields as needed
 	}
 	
 	public boolean hasNext() {
 		// rewrite...
-		return true;   
+		return current < arr.length; 
 	}
 
 	public E next() throws NoSuchElementException {
 		if (!hasNext())
-			throw new 
-				NoSuchElementException("No more elements to iterate over."); 
-		// rewrite
-		return null;
+			throw new NoSuchElementException("No more elements to iterate over."); 
+		E etr = arr[current];
+		current ++;
+		return etr;
 	}
 
 	public void remove() throws UnsupportedOperationException {
